aa3c39bdb1cb34793a613c29ea47c0e56e208730
Samvel Aslanyan <samaslanyan.work@gmail.com>
2026-08-11 18:38:05 +0400

    fix: removing fixed height styles and add div container inside header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,20 @@ import Controls from './Controls'
 export default function Header({ logoProps, navigationProps, controlsProps = true }) {
   return (
     <header className="header">
-      <div className="brand_container">
-        <img src={logoProps.src} alt={logoProps.alt} className="brand_logo" />
+      <div className="header_content">
+        <div className="brand_container">
+          <img src={logoProps.src} alt={logoProps.alt} className="brand_logo" />
+        </div>
+        <div className="navigation_container">
+          {navigationProps?.map((item) => (
+            <NavLink className="navigation_item" to={item.path} key={item.key}>
+              {item.label}
+            </NavLink>
+          ))}
+        </div>
+        {controlsProps && <Controls />}
       </div>
-      <div className="navigation_container">
-        {navigationProps?.map((item) => (
-          <NavLink className="navigation_item" to={item.path} key={item.key}>
-            {item.label}
-          </NavLink>
-        ))}
-      </div>
-      {controlsProps && <Controls />}
+      
     </header>
   )
 }
